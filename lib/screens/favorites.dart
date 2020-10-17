686673da01f483f96d51c4dc075401699a1eaac3
Marcelo Cardoso <marcelovicentegc@gmail.com>
2020-10-16 23:33:56 -0300

    feat: ability to toggle favorite comic image

--- lib/screens/favorites.dart
+++ lib/screens/favorites.dart
@@ -16,6 +16,8 @@ class FavoritesPage extends StatefulWidget {
 class _FavoritesPageState extends State<FavoritesPage> {
   List<String> favoriteComicsIds = new List<String>();
   List<Comic> favoriteComics = List<Comic>();
+  bool displayComic = false;
+  Comic currentComic;
   XkcdClient client;
   bool isLoading;
   Db db;
@@ -44,37 +46,68 @@ class _FavoritesPageState extends State<FavoritesPage> {
     });
   }
 
+  void handleOnPressed({comitToDisplay: Comic}) {
+    if (currentComic == null || !(currentComic.id == comitToDisplay.id)) {
+      setState(() {
+        displayComic = true;
+        currentComic = comitToDisplay;
+      });
+    } else {
+      setState(() {
+        displayComic = false;
+        currentComic = null;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(FAVORITES),
       ),
-      body: Center(
-        child: Padding(
-          padding: EdgeInsets.all(16.0),
-          child: Align(
-            alignment: Alignment.center,
-            child: Column(
-                children: isLoading
-                    ? [CircularProgressIndicator()]
-                    : favoriteComics
-                        .map(
-                          (favoriteComic) => Container(
-                            child: SizedBox(
-                              width: double.infinity,
-                              child: FlatButton(
-                                padding: EdgeInsets.all(8.0),
-                                onPressed: () => {print("PRESSED")},
-                                child: Text(
-                                  favoriteComic.title,
-                                  style: Theme.of(context).textTheme.headline6,
-                                ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Align(
+              alignment: Alignment.center,
+              child: Column(
+                  children: isLoading
+                      ? [CircularProgressIndicator()]
+                      : favoriteComics
+                          .map(
+                            (favoriteComic) => Container(
+                              child: Column(
+                                children: [
+                                  SizedBox(
+                                    width: double.infinity,
+                                    child: FlatButton(
+                                      padding: EdgeInsets.all(8.0),
+                                      onPressed: () => handleOnPressed(
+                                          comitToDisplay: favoriteComic),
+                                      child: Text(
+                                        favoriteComic.title,
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .headline6,
+                                      ),
+                                    ),
+                                  ),
+                                  displayComic &&
+                                          (currentComic.id == favoriteComic.id)
+                                      ? Container(
+                                          alignment: Alignment.center,
+                                          child:
+                                              Image.network(currentComic.img),
+                                        )
+                                      : Container()
+                                ],
                               ),
                             ),
-                          ),
-                        )
-                        .toList()),
+                          )
+                          .toList()),
+            ),
           ),
         ),
       ),
